430bf6fcab5f4171f9199382da120342e691ea00
andre-brdoch <andrebrdoch@yhahoo.de>
2024-02-11 12:24:43 +0100

    test: Add a dummy test
    Without any tests existing, our test script would fail. Adding a dummy test, just so that we can add the script already to our CI pipeline. Once we add proper tests in the future, the pipeline will be already set up. This dummy test can then be deleted.

---
+++ src/views/dummy-test.unit.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('placeholder tests', () => {
+  it('can be removed once we have at least 1 real test', () => {
+    expect(true).toBe(true)
+  })
+})
